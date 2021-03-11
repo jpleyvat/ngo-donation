a6ec266a7cde97667c48e76789da0ed15c354390
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-11 16:15:32 -0500

    Update urls.py

--- ngo/ngo/urls.py
+++ ngo/ngo/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('donations/', include('donations.urls')),
+    path('Users/', include('Users.urls')),
 ]
