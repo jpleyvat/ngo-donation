852e0ae9dc5abe60f64b99a992bce231072662f1
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-12 13:36:04 -0500

    Update models.py

--- ngo/Users/models.py
+++ ngo/Users/models.py
@@ -1,24 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 import uuid
 
+from .managers import CustUserManager
 # Create your models here.
-class User(models.Model):
-      ADMIN = 'Admin'
-      USER = 'Users'
-
-      USER_CHOICES = (
-          (ADMIN, 'Admin'),
-          (USER, 'Users'),
-      )
+class CustomUser(AbstractBaseUser, PermissionsMixin):
       UserID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
-      First_Name = models.CharField(max_length = 66, default = '')
-      Last_Name = models.CharField(max_length = 66, default = '')
-      Email = models.EmailField(max_length= 60)
+      first_name = models.CharField(_('first name '),max_length = 66, default = '')
+      last_name = models.CharField(_('last name'),max_length = 66, default = '')
+      email = models.EmailField(_('email address'), unique = True)
+      is_admin = models.BooleanField(_('is active'),default = False)
+      is_active = models.BooleanField(_('active'), default = True)
+      date_joined = models.DateTimeField(_('date joined'), default = timezone.now)
       Password = models.CharField(max_length = 60, default = '')
 
 # These fields are optional only require them to be filled out when user is donating for the first time.
 
-      CMA_Num = models.IntegerField(max_length = 80, blank = True, null = True,  default = '')
+      CMA_Num = models.IntegerField(blank = True, null = True,  default = '')
       Phone = models.CharField(max_length = 12, blank = True, default = '')
       AddressLineOne = models.CharField(max_length = 60, blank = True, null = True, default = '')
       AddressLineTwo = models.CharField(max_length = 60, blank = True, null = True, default = '')
@@ -28,5 +29,14 @@ class User(models.Model):
       Country = models.CharField(max_length = 60, blank = True,  null = True, default = '')
       Urbanization = models.CharField(max_length = 60, blank = True, null = True, default = '')
 
+
+      USERNAME_FIELD = 'email'
+      REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
+
+      objects = CustUserManager()
+
+      def __str__(self):
+            return self.email
+
       class Meta:
-            ordering = ['-Last_Name']
+            ordering = ['-last_name']
