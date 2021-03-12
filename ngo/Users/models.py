af37aeaa0aca223d26cde81d2325bd4909b7f584
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-12 18:55:23 -0500

    Update models.py
    Next: add views to link profile with user

--- ngo/Users/models.py
+++ ngo/Users/models.py
@@ -44,7 +44,18 @@ class CustomUser(AbstractUser):
             ordering = ['-last_name']
 
 
+class UserProfile(models.Model):
+      user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+      bio = models.TextField(max_length=500, blank=True)
+      location = models.CharField(max_length=30, blank=True)
+      birth_date = models.DateField(null=True, blank=True)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
