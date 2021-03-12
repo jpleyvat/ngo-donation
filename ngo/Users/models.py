8268f369c299cabba3472e4d63501f7429dab29d
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-12 18:55:55 -0500

    Update models.py
    forgot imports

--- ngo/Users/models.py
+++ ngo/Users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import uuid
 
 from .managers import UserManager
