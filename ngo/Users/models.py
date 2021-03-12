427901e0907012ac1faa13fb50c8b3e5f6bc2535
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-12 18:34:05 -0500

    Update models.py
    Todo: create user Profile

--- ngo/Users/models.py
+++ ngo/Users/models.py
@@ -1,25 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
-from django.utils import timezone
 import uuid
 
-from .managers import CustUserManager
+from .managers import UserManager
+
+
 # Create your models here.
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractUser):
       UserID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
       first_name = models.CharField(_('first name '),max_length = 66, default = '')
       last_name = models.CharField(_('last name'),max_length = 66, default = '')
       email = models.EmailField(_('email address'), unique = True)
-      is_admin = models.BooleanField(_('is active'),default = False)
-      is_active = models.BooleanField(_('active'), default = True)
-      date_joined = models.DateTimeField(_('date joined'), default = timezone.now)
-      Password = models.CharField(max_length = 60, default = '')
+      is_staff = models.BooleanField(_('is staff'),default = False)
+      is_active = models.BooleanField(_('is active'), default = True)
+      date_joined = models.DateTimeField(_('date joined'), auto_now_add = True)
+      password = models.CharField(max_length = 60, default = '')
 
 # These fields are optional only require them to be filled out when user is donating for the first time.
 
-      CMA_Num = models.IntegerField(blank = True, null = True,  default = '')
+      CMA_Num = models.IntegerField(blank = True, null = True,  default = 0)
       Phone = models.CharField(max_length = 12, blank = True, default = '')
       AddressLineOne = models.CharField(max_length = 60, blank = True, null = True, default = '')
       AddressLineTwo = models.CharField(max_length = 60, blank = True, null = True, default = '')
@@ -29,14 +29,22 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
       Country = models.CharField(max_length = 60, blank = True,  null = True, default = '')
       Urbanization = models.CharField(max_length = 60, blank = True, null = True, default = '')
 
-
       USERNAME_FIELD = 'email'
       REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
 
-      objects = CustUserManager()
+
+      objects = UserManager()
 
       def __str__(self):
             return self.email
 
       class Meta:
+            verbose_name = _('user')
+            verbose_name_plural = _('users')
             ordering = ['-last_name']
+
+
+
+
+
+
