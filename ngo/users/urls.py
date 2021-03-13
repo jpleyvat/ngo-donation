4374a65053ecea3b3860a202fff0ff23ff9dec23
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-13 16:32:21 -0500

    Update urls.py

--- ngo/users/urls.py
+++ ngo/users/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import create_user, delete_user, UsersListView
-app_name = 'Users'
+from .views import create_user, delete_user, UsersListView, UserUpdateView
 
+
+app_name = 'users'
 urlpatterns = [
     path('createUser/', create_user, name = 'Create_User' ),
     path('<pk>/deleteUser/', delete_user.as_view(), name = 'Delete_User'),
+    path('<pk>/updateUser/', UserUpdateView.as_view(), name = 'Update_User'),
     path('allusers/', UsersListView.as_view(), name='All_Users'),
 
 
