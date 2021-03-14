248d8ea0cb67c3bf15cd5a987248c2bec9986673
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-03-14 17:11:22 -0400

    Update forms.py

--- ngo/users/forms.py
+++ ngo/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import CustomUser, Profile
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 #Extend the UserCreationForm
@@ -8,19 +8,31 @@ class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = [
+            'username',
             'first_name',
             'last_name',
             'email',
-            'password',
             'is_staff',
         ]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control', 'size': '40'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_staff': forms.CheckboxInput(attrs={'class': 'form-control'}),
 
+        }
 
-class UpdateCustomUserForm(forms.ModelForm):
+class UpdateCustomUserForm(UserChangeForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
     Password = forms.CharField()
+
+class LoginForm(AuthenticationForm):
+    email = forms.CharField(label='Email ')
+
 
 
 class ProfileForm(forms.ModelForm):
@@ -30,7 +42,6 @@ class ProfileForm(forms.ModelForm):
             'bio',
             'location',
             'birth_date',
-            'cma_num',
             'phone',
             'addressLineOne',
             'addressLineTwo',
@@ -40,6 +51,22 @@ class ProfileForm(forms.ModelForm):
             'country',
             'urbanization',
         ]
+
+        widgets = {
+            'bio' : forms.Textarea(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'birth_date': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'addressLineOne': forms.TextInput(attrs={'class': 'form-control'}),
+            'addressLineTwo': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.TextInput(attrs={'class': 'form-control'}),
+            'urbanization': forms.TextInput(attrs={'class': 'form-control'}),
+
+
+        }
 
 
 class UpdateProfile(forms.ModelForm):
